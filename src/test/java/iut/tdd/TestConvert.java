83db76b20faf5665acfec06fdd3fbda3d1702ea2
Ali Douali <douali63@gmail.com>
2015-04-03 15:45:18 +0200

    C'est vert

--- src/test/java/iut/tdd/TestConvert.java
+++ src/test/java/iut/tdd/TestConvert.java
@@ -8,13 +8,13 @@ public class TestConvert {
 	@Test
 	public void test_num2text_zero () {
 		// Given
-		String input = "0";
+		/*String input = "0";
 		String expected = "zéro";
 		Convert myConvert = new Convert();
 		// When
-		String actual = myConvert.num2text(input);
+		String actual = myConvert.num2text(input);*/
 		// Then
-		Assert.assertEquals(expected, actual);
+		Assert.assertEquals("zéro", Convert.num2text("0"));
 	}
 	
 	@Test
