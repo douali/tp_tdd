74365c2317b2756dcf1122fed17f6a63a2183f14
Ali Douali <douali63@gmail.com>
2015-04-03 15:49:12 +0200

    Changement

--- src/main/java/iut/tdd/Convert.java
+++ src/main/java/iut/tdd/Convert.java
@@ -2,12 +2,12 @@ package iut.tdd;
 
 public class Convert {
 	public static String num2text(String input) {
-		if(input=="0")
+		if(input.equals("0"))
 			return "zéro";
 		return null;
 	}
 	public static String text2num(String input) {
-		if(input=="zéro")
+		if(input.equals("zéro"))
 			return "0";
 		return null;
 	}
