41609f53303236f76d5adec96b979a51c3d75794
Ali Douali <douali63@gmail.com>
2015-04-03 15:34:12 +0200

    Premier test

--- src/main/java/iut/tdd/Convert.java
+++ src/main/java/iut/tdd/Convert.java
@@ -2,6 +2,8 @@ package iut.tdd;
 
 public class Convert {
 	public static String num2text(String input) {
+		if(input=="0")
+			return "zéro";
 		return null;
 	}
 	public static String text2num(String input) {
