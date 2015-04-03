7e7e92c8e79d7639186eb36388eea1416da2327c
Ali Douali <douali63@gmail.com>
2015-04-03 16:38:52 +0200

    CA MARCHE

--- src/test/java/iut/tdd/TestConvert.java
+++ src/test/java/iut/tdd/TestConvert.java
@@ -7,19 +7,27 @@ import org.junit.Test;
 public class TestConvert {
 	@Test
 	public void test_num2text_zero () {
-		//Given
-		String input = "0";
-		String expected = "zéro";
-		Convert myConvert = new Convert();
-		//When
-		String actual = myConvert.num2text(input);
-		//Then
-		Assert.assertEquals(expected, actual);
+		Assert.assertEquals("zéro", Convert.num2text("0"));
 	}
 	
 	@Test
 	public void test_num2text_un() {
 		Assert.assertEquals("un", Convert.num2text("1"));
+	}
+	
+	@Test
+	public void test_num2text_deux() {
+		Assert.assertEquals("deux", Convert.num2text("2"));
+	}
+	
+	@Test
+	public void test_num2text_trois() {
+		Assert.assertEquals("trois", Convert.num2text("3"));
+	}
+	
+	@Test
+	public void test_num2text_quatre() {
+		Assert.assertEquals("quatre", Convert.num2text("4"));
 	}
 	
 	@Test
