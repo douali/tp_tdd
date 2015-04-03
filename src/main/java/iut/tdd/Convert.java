36d1a9fcd3659de5d71584514203ef702ace6b48
Ali Douali <douali63@gmail.com>
2015-04-03 16:36:56 +0200

    CA MARCHE

--- src/main/java/iut/tdd/Convert.java
+++ src/main/java/iut/tdd/Convert.java
@@ -1,10 +1,17 @@
 package iut.tdd;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Convert {
+		
 	public static String num2text(String input) {
-		if(input.equals("0"))
-			return "zéro";
-		return null;
+		String[] num0_9 = new String[]{"0","1","2","3","4","5","6","7","8","9"};
+		String[] text0_9 = new String[]{"zéro","un","deux","trois","quatre","cinq","six","sept","huit","neuf"};
+		Map<String, String> convert = new HashMap<String, String>();
+		for(int i = 0; i < 10; i++)
+			convert.put(num0_9[i], text0_9[i]);
+		return convert.get(input);
 	}
 	public static String text2num(String input) {
 		if(input.equals("zéro"))
--- src/test/java/iut/tdd/TestConvert.java
+++ src/test/java/iut/tdd/TestConvert.java
@@ -7,7 +7,19 @@ import org.junit.Test;
 public class TestConvert {
 	@Test
 	public void test_num2text_zero () {
-		Assert.assertEquals("zéro", Convert.num2text("0"));
+		//Given
+		String input = "0";
+		String expected = "zéro";
+		Convert myConvert = new Convert();
+		//When
+		String actual = myConvert.num2text(input);
+		//Then
+		Assert.assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void test_num2text_un() {
+		Assert.assertEquals("un", Convert.num2text("1"));
 	}
 	
 	@Test
